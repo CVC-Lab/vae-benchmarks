d72d7fb263b4566aad1dad702566f656569bfbcb
Ashwin V <ashwinnair2323@gmail.com>
2022-09-08 12:23:55 +0530

    mcevae config bug fix

--- src/pythae/models/mcevae/mcevae_config.py
+++ src/pythae/models/mcevae/mcevae_config.py
@@ -11,7 +11,7 @@ class MCEVAEConfig(VAEConfig):
     Parameters:
         input_dim (tuple): The input_data dimension.
         latent_dim (int): The latent space dimension. Default: None.
-        reconstruction_loss (str): The reconstruction loss to use ['bce', 'mse']. Default: 'mse'
+        reconstruction_loss (str): The reconstruction loss to use ['bce' 'mse']. Default: 'mse'
         beta (float): The balancing factor. Default: 10.
         C (float): The value of the KL divergence term of the ELBO we wish to approach, measured in
             nats. Default: 50.
@@ -24,14 +24,14 @@ class MCEVAEConfig(VAEConfig):
     latent_z_c: int = 0
     latent_z_var: int = 5
     mode: str = 'SO2'
-    invariance_decoder: str ='gated', 
-    rec_loss: str ='mse', 
-    div: str='KL',
-    in_dim: int = 1, 
-    out_dim: int = 1, 
-    hidden_z_c: int = 300,
-    hidden_z_var: int =300,
-    hidden_tau: int =32, 
-    training_mode: str = 'supervised',
-    device: str = 'cpu',
+    invariance_decoder: str ='gated'
+    rec_loss: str ='mse'
+    div: str='KL'
+    in_dim: int = 1
+    out_dim: int = 1
+    hidden_z_c: int = 300
+    hidden_z_var: int =300
+    hidden_tau: int =32 
+    training_mode: str = 'supervised'
+    device: str = 'cpu'
     tag: str = 'default'
